6eae2e8945386eaa755ca8ca5ebe1095b36df7cf
m-berner <martin.berner@gmx.net>
2025-10-15 19:43:14 +0200

    268

--- src/components/dialogs/AddStock.vue
+++ src/components/dialogs/AddStock.vue
@@ -6,45 +6,33 @@
   - Copyright (c) 2025-2025, Martin Berner, kontenmanager@gmx.de. All rights reserved.
   -->
 <script lang="ts" setup>
-import type {ICompanyData, IStockDB} from '@/types.d'
-import type {Ref} from 'vue'
-import {defineExpose, onMounted, reactive, ref} from 'vue'
+import type {IStockDB} from '@/types.d'
+import {defineExpose, onMounted, ref} from 'vue'
 import {useI18n} from 'vue-i18n'
 import {useApp} from '@/composables/useApp'
 import {useRuntime} from '@/composables/useRuntime'
 import {useSettings} from '@/composables/useSettings'
 import {useBrowser} from '@/composables/useBrowser'
 import {useStocksDB} from '@/composables/useIndexedDB'
-import {useFetch} from '@/composables/useFetch'
 import {useValidation} from '@/composables/useValidation'
 import {useRecordsStore} from '@/stores/records'
-
-interface INewStock {
-  isin: string,
-  company: string,
-  symbol: string
-}
+import {useStockFormular} from '@/composables/useStockFormular'
+import StockFormular from '@/components/dialogs/forms/StockFormular.vue'
 
 const {t} = useI18n()
 const {log} = useApp()
 const {notice} = useBrowser()
 const {addStock} = useStocksDB()
-const {fetchCompanyData} = useFetch()
-const {isinRules, validateForm} = useValidation()
+const {validateForm} = useValidation()
 const {activeAccountId} = useSettings()
 const {resetTeleport} = useRuntime()
 const records = useRecordsStore()
+const {stockFormularData, formRef} = useStockFormular()
 
-const newStock: INewStock = reactive({
-  isin: '',
-  company: '',
-  symbol: ''
-})
-const formRef: Ref<HTMLFormElement | null> = ref(null)
 const formDisabled = ref(false)
 
 const reset = (): void => {
-  Object.assign(newStock, {
+  Object.assign(stockFormularData, {
     isin: '',
     company: '',
     symbol: ''
@@ -52,23 +40,14 @@ const reset = (): void => {
   formDisabled.value = false
 }
 
-const onUpdateIsin = async (): Promise<void> => {
-  if (newStock.isin !== '' && newStock.isin?.length === 12) {
-    const fetchedCompanyData: ICompanyData = await fetchCompanyData(newStock.isin)
-    newStock.company = fetchedCompanyData.company
-    newStock.symbol = fetchedCompanyData.symbol.toUpperCase()
-  }
-}
-
 const onClickOk = async (): Promise<void> => {
   log('ADD_STOCK : onClickOk')
   if (!await validateForm(formRef)) return
-  log('AFTEROK')
   try {
     const stock: Omit<IStockDB, 'cID'> = {
-      cCompany: newStock.company.trim(),
-      cISIN: newStock.isin,
-      cSymbol: newStock.symbol,
+      cCompany: stockFormularData.company.trim(),
+      cISIN: stockFormularData.isin,
+      cSymbol: stockFormularData.symbol,
       cMeetingDay: '',
       cQuarterDay: '',
       cFadeOut: 0,
@@ -88,7 +67,6 @@ const onClickOk = async (): Promise<void> => {
     await notice([t('dialogs.addStock.error')])
   }
 }
-
 const title = t('dialogs.addStock.title')
 
 defineExpose({onClickOk, title})
@@ -108,35 +86,6 @@ log('--- AddStock.vue setup ---')
       ref="formRef"
       validate-on="submit"
       @submit.prevent>
-    <v-text-field
-        v-model="newStock.isin"
-        :counter="12"
-        :label="t('dialogs.addStock.isin')"
-        :rules="isinRules([
-              t('validators.isinRules.required'),
-              t('validators.isinRules.length'),
-              t('validators.isinRules.format'),
-              t('validators.isinRules.country'),
-              t('validators.isinRules.luhn'),
-          ])"
-        autofocus
-        variant="outlined"
-        @focus="formRef?.resetValidation()"
-        @update:modelValue="onUpdateIsin"/>
-    <v-switch
-        v-model="formDisabled"
-        :label="t('dialogs.addStock.formDisabledLabel')"
-        color="red"
-        variant="outlined"/>
-    <v-text-field
-        v-model="newStock.company"
-        :disabled="!formDisabled"
-        :label="t('dialogs.addStock.company')"
-        variant="outlined"/>
-    <v-text-field
-        v-model="newStock.symbol"
-        :disabled="!formDisabled"
-        :label="t('dialogs.addStock.symbol')"
-        variant="outlined"/>
+    <StockFormular :isUpdate="false"/>
   </v-form>
 </template>
